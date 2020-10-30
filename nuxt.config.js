8b94a609de3cff0e4e72ab281b27619be95c09e9
Josep Jaume Rey Peroy <josepjaume@codegram.com>
2020-10-30 14:12:34 +0000

    Disable nuxt pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,8 +32,6 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
