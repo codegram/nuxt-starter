19ca8f51d059e2a7a904ac204324d284eb0e946b
Josep Jaume Rey Peroy <josepjaume@codegram.com>
2021-05-25 21:00:11 +0200

    Fix footer test

---
+++ test/Footer.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/vue'
+import Footer from '@/components/layout/LayoutFooter.vue'
+
+describe('Logo', () => {
+  test('is a Vue instance', () => {
+    const { getByText }= render(Footer)
+    getByText("Footer")
+  })
+})
