b64feadaa81203f2d8d98f2ccbeb421737cb939c
Josep Jaume Rey Peroy <josepjaume@codegram.com>
2021-07-29 14:55:48 +0200

    Add lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: 'dist',
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 0.95 }],
+        'uses-text-compression': 'off',
+        'csp-xss': 'off',
+        'unused-javascript': 'off',
+        'legacy-javascript': 'off',
+        'largest-contentful-paint': ['error', { minScore: 0.8 }],
+      },
+    },
+    upload: {
+      target: 'filesystem',
+      outputDir: 'lighthouse-report',
+    },
+  },
+}
